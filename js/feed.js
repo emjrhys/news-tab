f8a764da179883998b632d295ae88e02fb9bde53
Noah Laferty <noah.lafferty@gmail.com>
2015-01-14 00:02:35 +0100

    grid, hover, working links

--- js/feed.js
+++ js/feed.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-    var FEED_URL = "https://news.google.com/news?pz=1&cf=all&topic=tc&output=rss";
+    // https://news.google.com/news?pz=1&cf=all&topic=w&output=rss
+    var FEED_URL = "https://news.google.com/news?output=rss";
+    
+//    $(function() {
+//        $('#news article').each(function(i) {
+//            $(this).delay((i++) * 500).fadeTo(1000, 1); 
+//        });
+//    });
+    
+    function getPublishedDate(e) {
+        var currTime = new Date();
+        var pubDate = new Date(e.publishedDate);
+        
+        var str;
+        
+        var elapsedTime = Math.floor((currTime.getTime() - pubDate.getTime())/60000);
+        if (elapsedTime < 60)
+            str = elapsedTime + " minutes ago";
+        else if (elapsedTime/1440 > 1)
+            str = elapsedTime/1440 + " days ago";
+        else {
+            elapsedTime = Math.floor(elapsedTime/60);
+            str = elapsedTime;
+            if (elapsedTime == 1)
+                str += " hour ago";
+            else
+                str += " hours ago";
+        }
+        
+        return str;
+    }
     
     $.ajax({
         type: 'GET',
@@ -7,17 +37,22 @@ $(document).ready(function() {
         dataType : 'json',
         success  : function (data) {
             if (data.responseData.feed && data.responseData.feed.entries) {
-//                $.each(data.responseData.feed.entries, function (i, e) {
-//                    console.log("------------------------");
-//                    console.log("title         :    " + e.title);
-//                    console.log("snippet       :    " + e.contentSnippet);
-//                    console.log("publishedDate :    " + e.publishedDate);
-//                });
                 console.log(data.responseData.feed.entries);
                 $.each(data.responseData.feed.entries, function (i, e) {
-                    var c = e.content;
-//                    console.log($(c).find("img").attr("src"));
-                    $('#news').append("<img src=\"https:" + $(c).find("img").attr("src") + "\">");
+                    var d = getPublishedDate(e);
+                    var c = $(e.content);
+                    var imgSrc = c.find('img').attr('src');
+                    
+                    var ar = $('<a></a>')
+                        .attr('href', e.link)
+                        .append('<h3>' + e.title + '</h3')
+                        .append('<span class="date">' + d + '</span>');
+//                        .append('<p>' + e.contentSnippet + '</p>')
+                    
+                    if (imgSrc != undefined)
+                        ar.append('<img src="https:' + imgSrc + '">');
+                    
+                    $('#news').append(ar);
                 });
             }
         }
